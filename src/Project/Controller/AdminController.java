ebef1e9551c61542c5d8bb4714e04846bd4b898c
antonsjostrand <anton.sjostrand@hotmail.se>
2018-04-18 16:19:39 +0200

    Added new controllers and scenes

---
+++ src/Project/Controller/AdminController.java
@@ -0,0 +1,14 @@
+package Project.Controller;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AdminController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
--- src/Project/Controller/LoginController.java
+++ src/Project/Controller/LoginController.java
@@ -21,8 +21,7 @@ import java.util.ResourceBundle;
 
 public class LoginController implements Initializable {
 
-    @FXML
-    private ImageView startBackground;
+    @FXML private ImageView startBackground;
     @FXML private AnchorPane anchorPane;
     @FXML private Button guestButton;
     @FXML private TextField userNameTF;
@@ -33,32 +32,79 @@ public class LoginController implements Initializable {
     @FXML private Button lostPasswordButton;
 
     @FXML void adminButtonPressed(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Project/View/adminScene.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e){
+            //Fixa error handling
+        }
     }
 
     @FXML void createAccountButtonPressed(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Project/View/createAccountScene.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e){
+            //Fixa error handling
+        }
     }
 
     @FXML void guestButtonPressed(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Project/View/guestScene.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e){
+            //Fixa error handling
+        }
     }
 
-    @FXML void loginButtonPressed(ActionEvent event) throws IOException {
+    @FXML void loginButtonPressed(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
 
-        Node node = (Node) event.getSource();
-        Stage stage = (Stage) node.getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Project/View/userScene.fxml"));
+            Parent root = loader.load();
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Project/View/userScene.fxml"));
-        Parent root = loader.load();
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        } catch (IOException e){
+            //Fixa error handling
+        }
     }
 
 
     @FXML void lostPasswordButtonPressed(ActionEvent event) {
+        try {
+            Node node = (Node) event.getSource();
+            Stage stage = (Stage) node.getScene().getWindow();
 
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Project/View/lostPasswordScene.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+        }catch (IOException e){
+            //Fixa error handling
+        }
     }
 
     @Override
